02c460e97646d579fbd11159304e3c1eda3422de
Paul Han <paul.han0822@gmail.com>
2023-03-26 22:10:47 -0700

    initialize repository

---
+++ app.py
@@ -0,0 +1,29 @@
+import yfinance as yf
+
+# Get the user's input for the company ticker symbol
+ticker = input("Enter the company ticker symbol: ")
+
+# Fetch the financial data for the ticker symbol using yfinance
+stock = yf.Ticker(ticker)
+
+# Calculate the financial ratios
+pe_ratio = stock.info['trailingPE']
+ps_ratio = stock.info['priceToSalesTrailing12Months']
+pb_ratio = stock.info['priceToBook']
+earnings_yield = 1 / pe_ratio
+ev_to_ebitda = stock.info['enterpriseValue'] / stock.info['ebitda']
+fcf_ratio = stock.info['freeCashFlow'] / stock.info['marketCap']
+roe = stock.info['returnOnEquity']
+roa = stock.info['returnOnAssets']
+roce = stock.info['returnOnCapitalEmployed']
+
+# Print the financial ratios
+print("PE ratio: ", pe_ratio)
+print("PS ratio: ", ps_ratio)
+print("PB ratio: ", pb_ratio)
+print("Earnings yield: ", earnings_yield)
+print("EV-to-EBITDA ratio: ", ev_to_ebitda)
+print("FCF ratio: ", fcf_ratio)
+print("ROE: ", roe)
+print("ROA: ", roa)
+print("ROCE: ", roce)
