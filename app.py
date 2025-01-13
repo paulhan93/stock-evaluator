4823fda30534d861bef4244eecc78fcf6674d5c1
Paul Han <paul.han0822@gmail.com>
2025-01-12 17:49:16 -0800

    add main function to script

--- app.py
+++ app.py
@@ -5,50 +5,63 @@
 import yfinance as yf
 import json
 
-# Prompt user for ticker symbol
-ticker = input("Ticker: ")
-print("\n")
+def main():
+    # Prompt user for ticker symbol
+    ticker = input("Ticker: ")
 
-# Fetch the financial data for the ticker symbol using yfinance
-stock = yf.Ticker(ticker)
-info = stock.info
+    # Fetch the financial data for the ticker symbol using yfinance
+    stock = yf.Ticker(ticker)
+    info = stock.info
 
-# Save the info dictionary to a file
-file_name = f"{ticker}_info.json"
-with open(file_name, "w") as file:
-    json.dump(info, file, indent=4)
+    # Save the info dictionary to a file
+    file_name = f"{'ticker'}_info.json"
+    with open(file_name, "w") as file:
+        json.dump(info, file, indent=4)
 
-# print stock info
-#print(stock.info)
+    # print company info
+    print(stock.info['website'])
+    print(stock.info['address1'])
+    print(stock.info['city'], stock.info['state'])
+    print(stock.info['zip'])
+    print(stock.info['country'])
+    print()
+    print(stock.info['industry'])
+    print(stock.info['sector'])
+    print()
+    print(stock.info['longBusinessSummary'])
+    print()
 
-#print(stock.history(period="1mo"))
-
-#print(stock.financials)
-
-#print(stock.balance_sheet)
-
-#print(stock.cashflow)
+    # print current stock price
+    print("Current Stock Price: ", stock.info['currentPrice'])
+    print()
+    
+    # print financial ratios
 
 
-# Calculate Financial Ratios: These ratios are used to evaluate the value of a stock 
-pe_ratio = stock.info['trailingPE']
-ps_ratio = stock.info['priceToSalesTrailing12Months']
-pb_ratio = stock.info['priceToBook']
-earnings_yield = 1 / pe_ratio
-ev_to_ebitda = stock.info['enterpriseValue'] / stock.info['ebitda']
-#fcf_ratio = stock.info['freeCashFlow'] / stock.info['marketCap']
-roe = stock.info['returnOnEquity']
-roa = stock.info['returnOnAssets']
-#roce = stock.info['returnOnCapitalEmployed']
+    # Calculate Financial Ratios: These ratios are used to evaluate the value of a stock 
+    pe_ratio = stock.info['trailingPE']
+    ps_ratio = stock.info['priceToSalesTrailing12Months']
+    pb_ratio = stock.info['priceToBook']
+    earnings_yield = 1 / pe_ratio
+    ev_to_ebitda = stock.info['enterpriseValue'] / stock.info['ebitda']
+    #fcf_ratio = stock.info['freeCashFlow'] / stock.info['marketCap']
+    roe = stock.info['returnOnEquity']
+    roa = stock.info['returnOnAssets']
+    #roce = stock.info['returnOnCapitalEmployed']
 
-# Print Financial Ratios
-print("\n")
-print("PE ratio: ", pe_ratio)
-print("PS ratio: ", ps_ratio)
-print("PB ratio: ", pb_ratio)
-print("Earnings yield: ", earnings_yield)
-print("EV-to-EBITDA ratio: ", ev_to_ebitda)
-#print("FCF ratio: ", fcf_ratio)
-print("ROE: ", roe)
-print("ROA: ", roa)
-#print("ROCE: ", roce)
+    # Print Financial Ratios
+    print("PE ratio: ", pe_ratio)
+    print("PS ratio: ", ps_ratio)
+    print("PB ratio: ", pb_ratio)
+    print("Earnings yield: ", earnings_yield)
+    print("EV-to-EBITDA ratio: ", ev_to_ebitda)
+    #print("FCF ratio: ", fcf_ratio)
+    print("ROE: ", roe)
+    print("ROA: ", roa)
+    #print("ROCE: ", roce)
+
+    return 0
+
+# main
+if __name__ == "__main__":
+    main()
